f6431cde42c2be43c06121e6d0de6fdfe34f47a5
Joris Vandeweerd <joris@statik.be>
2014-01-21 17:23:07 +0100

    added security manager with the current request for better handling of isUrlAllowed

---
+++ src/pallo/web/security/SecurityManager.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace pallo\web\security;
+
+use pallo\library\http\Request;
+use pallo\library\security\SecurityManager as LibSecurityManager;
+
+class SecurityManager extends LibSecurityManager {
+
+    public function setRequest(Request $request) {
+        $this->request = $request;
+    }
+
+    /**
+     * Checks whether the current user is allowed to view the provided URL
+     * @param string $url URL to check
+     * @return boolean
+     */
+    public function isUrlAllowed($url) {
+        if (!$this->request) {
+            return parent::isUrlAllowed();
+        }
+
+        $baseUrl = $this->request->getBaseUrl();
+        $path = str_replace($baseUrl, '', $url);
+
+        if (strpos($path, '?') !== false) {
+            list($path, $query) = explode('?', $path, 2);
+        }
+
+        return $this->isPathAllowed($path);
+    }
+
+}
